290d1fd068019d7c2b578cfcf9aa44c31da9c6f8
hamza1811 <hk370019@gmail.com>
2020-02-12 18:21:05 +0530

    Done

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+
+var message = require('./page1');
+require('../css/demo.scss');
+alert(message);
+console.log("hi");	
+
+const hamza="Hamza khan";
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const CompressionPlugin = require('compression-webpack-plugin');
+
+
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './javascript/index.js',
+
+  plugins: [new CompressionPlugin()],
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    },
+    {
+       test: /\.scss$/,
+       loader: 'style-loader!css-loader!sass-loader'
+    }
+]
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript 
+  // minifying and other thing so let's set mode to development
+  mode: 'development'
+};
